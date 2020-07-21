adabee7ff7933740acc07c574771e37742804712
margarita-888 <rl@eworkspace.com.au>
2020-07-21 15:35:51 +1000

    added logo to assets folder, added header component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {Link } from 'react-router-dom';
+import { ReactComponent as Logo} from '../../assets/crown.sgv';
+
+import './header.styles.scss';
+
+const Header = () => (
+<div className='header'>
+<Link to='/' >
+<Logo />
+</Link>
+
+</div>
+);
+
+export default Header;
